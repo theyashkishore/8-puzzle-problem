de5c771af19eb6dbfb33930ed00224d14d767151
Yash Kishore <yashkishore@Yash-MacBook-Air.local>
2023-11-09 23:06:29 +0530

    testing github action workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,28 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    plugins: ['commitlint-plugin-jira-rules'], // Load custom plugin
-    rules: {
-      'jira-rules/user-story-number': [2, 'always', /^(US\d{7})$/, 'Error: User story must be in the "USxxxxxxx" format.'],
-    },
-  };
+// module.exports = {
+//     extends: ['@commitlint/config-conventional'],
+//     plugins: ['commitlint-plugin-jira-rules'], // Load custom plugin
+//     rules: {
+//       'jira-rules/user-story-number': [2, 'always', /^(US\d{7})$/, 'Error: User story must be in the "USxxxxxxx" format.'],
+//     },
+//   };
   
+// commitlint.config.js
+
+module.exports = {
+    rules: {
+      'jira-rules/user-story-number': [2, 'always'],
+    },
+    plugins: [
+      {
+        rules: {
+          'jira-rules/user-story-number': ({subject}) => {
+            const userStoryPattern = /^US\d{7}$/ ;
+            return [
+              subject.includes(HELLO_WORLD),
+              `Enter Valid Commit Message`,
+            ];
+          },
+        },
+      },
+    ],
+  };
