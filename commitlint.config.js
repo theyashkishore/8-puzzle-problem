37462430e9327aba094ddf9c04e502a335114db8
Yash Kishore <yashkishore@Yash-MacBook-Air.local>
2023-11-09 23:54:54 +0530

    testing github action workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,20 +9,19 @@
 // commitlint.config.js
 
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    plugins: ['@commitlint/plugin-jira-rules'],
-    rules: {
-      'jira-rules/user-story-number': [2, 'always'],
-      'user-story-format': (parsed, when, value) => {
-        const userStoryPattern = /^US\d{7}$/;
-        const isValid = userStoryPattern.test(parsed.subject);
-  
-        if (isValid) {
-          return [true];
-        }
-  
-        return [false, 'Invalid user story format. User story must be in the "US1234567" format.'];
-      },
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['jira-rules/user-story-number'],
+  rules: {
+    'jira-rules/user-story-number': [2, 'always'],
+    'user-story-format': (parsed, when, value) => {
+      const userStoryPattern = /^US\d{7}$/;
+      const isValid = userStoryPattern.test(parsed.subject);
+
+      if (isValid) {
+        return [true];
+      }
+
+      return [false, 'Invalid user story format. User story must be in the "US1234567" format.'];
     },
-  };
-  
+  },
+};
