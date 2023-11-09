9c6b8f243adffc436c14d9c986644ad4dff3c637
Yash Kishore <yashkishore@Yash-MacBook-Air.local>
2023-11-09 21:26:55 +0530

    testing github action workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,10 @@
-module.exports = {
+module.exports = {  
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'subject': [
-        2,
-        'always',
-        /^US\d{7}/,
-        'Commit message must contain a user story starting with "US" followed by 7 digits.'
-      ],
-    },
+      'type-enum': [2, 'always', ['userstory']], // Optional: Define allowed commit types
+      'type-case': [2, 'always', 'kebab-case'], // Optional: Enforce kebab-case for commit types
+      'type-empty': [2, 'never'], // Optional: Require a valid commit type
+      'subject': [2, 'always', /^US\d{7}/, '9-digit user story number required'],
+    }
   };
   
